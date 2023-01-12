7fafc0849f67d43256148714ea6bfff22ffc3b44
ayensi <okanaltun9@gmail.com>
2023-01-12 15:00:30 +0300

    mail scheduling

---
+++ src/main/java/com/altun/fiveten/dto/EmailRequestDTO.java
@@ -0,0 +1,35 @@
+package com.altun.fiveten.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailRequestDTO {
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String subject;
+
+    @NotEmpty
+    private String body;
+
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @NotNull
+    private ZoneId timeZone;
+
+
+}
+
